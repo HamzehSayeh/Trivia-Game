5d580258518a7116020009091be9367fbba9c7e9
Hamzeh Sayeh <hamzehsayeh6@gmail.com>
2024-12-06 21:14:35 +0200

    adding game logic

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/formatMessage.js");
+
+const {
+  addPlayer,
+  getAllPlayers,
+  getPlayer,
+  removePlayer,
+} = require("./utils/players.js");
+const { error } = require("console");
 
 const app = express();
 
@@ -13,8 +22,17 @@ const io = socketio(server);
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log(`new user connected`);
+  socket.on("join", ({ playerName, room }, callback) => {
+    const { error, newPlayer } = addPlayer({ playerName, room, id: socket.id });
+    if (error) return callback(error.message);
+    callback();
+
+    socket.join(newPlayer.room);
+
+    socket.emit("message", formatMessage("Admin", "Welcome!"));
+  });
 });
 
 server.listen(port, () => {
---
+++ src/utils/players.js
@@ -0,0 +1,61 @@
+const players = [];
+
+const addPlayer = ({ id, playerName, room }) => {
+  if (!playerName || !room)
+    return {
+      error: new Error("Please Provide Player Name and Room"),
+    };
+
+  playerName = playerName.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const existingPlayer = players.find((player) => {
+    return player.playerName === playerName && player.room === room;
+  });
+
+  if (existingPlayer) {
+    return {
+      error: new Error("Player name in use!"),
+    };
+  }
+  const newPlayer = { id, playerName, room };
+  players.push(newPlayer);
+
+  return { newPlayer };
+};
+
+const findPlayerById = (id) => {
+  const player = players.find((player) => {
+    player.id === id;
+  });
+
+  if (!player) {
+    return {
+      error: new Error("Player not found"),
+    };
+  }
+
+  return { player };
+};
+
+const getAllPLayers = (room) => {
+  return players.filter((player) => {
+    player.room === room;
+  });
+};
+
+const removePlayer = (id) => {
+  players.find((player, index) => {
+    if (player.id === id) {
+      return players.splice(index, 1)[0];
+    }
+    return false;
+  });
+};
+
+module.exports = {
+  addPlayer,
+  getAllPLayers,
+  findPlayerById,
+  removePlayer,
+};
